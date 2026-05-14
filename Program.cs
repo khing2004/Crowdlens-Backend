151b98c66b0cdd1bb9d03ba94141348ee749281b
khing2001 <caritero.khingiankeith1@gmail.com>
2026-05-14 15:41:10 +0800

    Added frontent link to CORS

--- Program.cs
+++ Program.cs
@@ -17,7 +17,8 @@ builder.Services.AddCors(options =>
         {
             policy.WithOrigins(
                 "http://localhost:5173",
-                "https://crowdlens-frontend.vercel.app" // replace with your actual Vercel URL
+                "https://crowdlens-frontend.vercel.app", // replace with your actual Vercel URL
+                "https://crowd-lens-frontend.vercel.app/"
             )
                 .AllowAnyHeader()
                 .AllowAnyMethod();
